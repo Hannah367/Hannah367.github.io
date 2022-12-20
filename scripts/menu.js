291566c726dc0a84b2cd3277ce6004e77eaeb173
Matt Stutely <matt@stutelys.me.uk>
2022-12-20 18:46:08 +0000

    updates to homepage etc etc

---
+++ scripts/menu.js
@@ -0,0 +1,31 @@
+
+document.write('<nav class="navbar navbar-expand-lg bg-light"><div class="container-fluid"><a class="navbar-brand" href="index.html">Hannah Stutely : Portfolio</a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent">');
+document.write('<ul class="navbar-nav me-auto mb-2 mb-lg-0">');
+document.write('<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>');
+
+document.write('<li class="nav-item"><a class="nav-link" href="about.html">About Me</a></li>');
+
+document.write('<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Highlights</a>');
+document.write('<ul class="dropdown-menu">');
+document.write('<li><a class="dropdown-item" href="safetyapp.html">Night Owl</a></li>');
+document.write('<li><a class="dropdown-item" href="nanolife.html">NanoLife</a></li>');
+document.write('<li><a class="dropdown-item" href="uxdTrain.html">NRM Train Exhibit</a></li>');
+document.write('<li><a class="dropdown-item" href="partyanimals.html">Party Animals: Hospital Rush</a></li>');
+document.write('<li><a class="dropdown-item" href="cricket.html">Cricket Analyse</a></li>');
+document.write('<li><a class="dropdown-item" href="nft.html">PartyPuses NFT</a></li>');
+document.write('</ul>');
+document.write('</li>');
+
+document.write('<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Other Projects</a>');
+document.write('<ul class="dropdown-menu">');
+document.write('<li><a class="dropdown-item" href="cooperate.html">Cooperate</a></li>');
+document.write('<li><a class="dropdown-item" href="dayinthelife.html">A Day In The Life</a></li>');
+document.write('<li><a class="dropdown-item" href="woods.html">Lost in the Woods</a></li>');
+document.write('<li><a class="dropdown-item" href="disney.html">Disney World Website</a></li>');
+document.write('<li><a class="dropdown-item" href="polarbear.html">Polar Bear News</a></li>');
+document.write('<li><a class="dropdown-item" href="holiday.html">Holiday Videos</a></li>');
+document.write('</ul>');
+document.write('</li>');
+
+document.write('</ul>');
+document.write('</div></div></nav>');
